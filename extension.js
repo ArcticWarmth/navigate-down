aa545cb9d87e53243f3e40fa8e5dd93449049b54
ArcticWarmth <ArcticWarmthDev@protonmail.com>
2025-08-04 22:51:23 -0500

    fix: fix logic errors in primary extension file

--- extension.js
+++ extension.js
@@ -11,18 +11,22 @@ function activate(context) {
 	context.subscriptions.push(vscode.commands.registerCommand("navigate-down.open", (url) => {
 		try {
 			// Prevent opening the current directory
-			let folders = vscode.workspace.workspaceFolders[0];
-			let workspace = false;
+			let folders = vscode.workspace.workspaceFolders;
 			// Check if user is in a workspace
-			if (folders != undefined && (folders.uri.fsPath == url.fsPath)) {
-				vscode.window.showErrorMessage("Cannot navigate to current directory");
-				return;
-			} else if (folders == undefined || !existsSync(url.fsPath) ) {
-				throw new ReferenceError("Attempted to check or navigate to non-existent directory")
+			if (folders != undefined && existsSync(url.fsPath)) {
+				folders.forEach(folders => {
+					if (folders.uri == url.fsPath) {
+						vscode.window.showErrorMessage("Cannot navigate to currently open directory workspace");
+						return;
+					}
+				});
+					vscode.window.showInformationMessage("Opening " + url.fsPath);
+					vscode.commands.executeCommand('vscode.openFolder', url);	
+			} else if (folders == undefined && existsSync(url.fsPath)) {
+				vscode.window.showInformationMessage("Opening " + url.fsPath);
+				vscode.commands.executeCommand('vscode.openFolder', url);	
 			} else {
-				vscode.showInformationMessage("Opening " + url.fsPath);
-				vscode.commands.executeCommand('vscode.openFolder', url,);
-
+				throw new ReferenceError("Attempted to check or navigate to non-existent directory")
 			}
 		} catch (error) {
 			console.error(error)
