406b9ae1a359b7db9c2a7b35770e8e70c0a87f86
ArcticWarmth <ArcticWarmthDev@protonmail.com>
2026-03-20 11:51:28 -0500

    chore: fix eslint memory leak

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,34 @@
 import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-export default [{
-    files: ["**/*.js"],
-    languageOptions: {
-        globals: {
-            ...globals.commonjs,
-            ...globals.node,
-            ...globals.mocha,
+export default defineConfig([
+    {
+        ignores: [
+            "node_modules/",
+            ".vscode*",
+            "*.config.mjs",
+            "test/"
+        ]
+    },
+    {
+        files: ['extension.js'],
+        languageOptions: {
+            globals: {
+                ...globals.commonjs,
+                ...globals.node,
+                ...globals.mocha
+            },
+            sourceType: "module",
+            ecmaVersion: 2022
         },
-
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
-
-    rules: {
-        "no-const-assign": "warn",
-        "no-this-before-super": "warn",
-        "no-undef": "warn",
-        "no-unreachable": "warn",
-        "no-unused-vars": "warn",
-        "constructor-super": "warn",
-        "valid-typeof": "warn",
-    },
-}];
+        rules: {
+            "no-const-assign": "warn",
+            "no-this-before-super": "warn",
+            "no-undef": "warn",
+            "no-unreachable": "warn",
+            "no-unused-vars": "warn",
+            "constructor-super": "warn",
+            "valid-typeof": "warn",
+        }
+    }
+])
